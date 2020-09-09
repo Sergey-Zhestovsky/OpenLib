d18331095d4c51db83b643dcc1811af6d4606c95
Sergey Zhestovsky <Sergey.play123@gmail.com>
2020-09-10 02:43:19 +0300

    create client boilerplate

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.scss';
+
+function App() {
+  return (
+    <div className={styles["App"]}>
+      lets GOOOOOOOOOOO
+    </div>
+  );
+}
+
+export default App;
