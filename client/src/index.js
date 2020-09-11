e934eb7b4fd25bfdfef4f6875d25707b4cfaaebb
Sergey Zhestovsky <Sergey.play123@gmail.com>
2020-09-11 07:39:51 +0300

    add fonts

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+import './css/fonts.scss';
 import './css/index.scss';
+import 'normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
